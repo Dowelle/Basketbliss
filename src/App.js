d1b6c55ab6a70979c892360be6aaee9299afbedf
Riandrei Casanas <riandrei.casanas@gmail.com>
2023-11-20 14:56:19 +0800

    Sign up working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import './services/firebase'
+
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import Landing from './pages/Landing';
 import Signin from './pages/Signin';
 import Registration from './pages/Registration';
 import Homepage from './pages/Homepage';
 import Settings from './pages/Settings';
+
 function App() {
   return (
     <div className="App">
---
+++ src/services/firebaseActions.js
@@ -0,0 +1,85 @@
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc, updateDoc} from "firebase/firestore"
+
+const db = getFirestore();
+const auth = getAuth()
+
+export const signUpMerchantWithEmailAndPassword = (email, password) => {
+	
+	return createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+    // Signed up
+    const user = userCredential.user;
+    console.log(user)
+    setDoc(doc(db, "merchants", user.uid), {})
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    
+    console.log(errorCode, errorMessage);
+    return errorCode;
+  });
+}
+
+export const logInWithEmailAndPassword = (email, password) => {
+  
+  
+  return signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    
+    console.log(errorCode, errorMessage);
+    return errorCode;
+  });
+}
+
+export const signOutUser = () => {
+  return signOut(auth).then(() => {
+    return true;
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    
+    console.log(errorCode, errorMessage);
+    return false;
+  })
+}
+export const updateMerchantDetails = (merchantDetails, merchantId) => {
+  const merchantRef = doc(db, "merchants", merchantId)
+  
+  return updateDoc(merchantRef, merchantDetails).then((payload) => {
+    console.log(payload)
+    return true;
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message
+    return false;
+  })
+}
+
+export const getMerchantDetails = (merchantId) => {
+  const docRef = doc(db, "merchants", merchantId);
+  
+  return getDoc(docRef).then(payload => {
+    const data = payload._document.data.value.mapValue.fields;
+    
+    return data;
+  })
+}
+
+onAuthStateChanged(auth, (user) => {
+  console.log("test")
+  console.log(user)
+  if(user) {
+    sessionStorage.setItem("uid", user.uid)
+  } else {
+    sessionStorage.clear()
+  }
+})
+
